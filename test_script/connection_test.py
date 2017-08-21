884d823e33f91fd4ba6683d6753fc115d6b2ed29
panyixiao <panyixiao123@gmail.com>
2017-08-21 23:26:54 +0800

    demo_final version

--- test_script/connection_test.py
+++ test_script/connection_test.py
@@ -3,7 +3,7 @@ import sys, os
 from time import sleep
 # from .log import DEBUG, INFO, ERROR, printf, init_logger, set_default_logger, close_logger
 
-vel = 3;
+sleepTime = 3;
 
 uArm = pyuarm.UArm()
 uArm.connect()
@@ -17,32 +17,32 @@ while True:
 
 	print("Move to Picking Point (Above)");
 	uArm.set_position(100, 150, 150, speed = 100);
-	sleep(vel);
+	sleep(sleepTime);
 
 	print("Move to Picking Point");
 	uArm.set_position(100, 150, 55, speed = 30);
 	print("Pick !!!");
 	uArm.set_pump(True);
-	sleep(vel);
+	sleep(sleepTime);
 
 	print("Move to Picking Point (Above)");
-	uArm.set_position(100, 150, 150, speed = 5,wait = True);
-	sleep(vel);
+	uArm.set_position(100, 150, 150, speed = 1.0,wait = True);
 
 	print("Move to Dropping Point (Above)");
 	uArm.set_position(-100, 150, 150, speed = 50, wait = True);
 	uArm.set_wrist(160)
-	sleep(vel);
 
 	print("Move to Dropping Point");
 	uArm.set_position(-100, 150, 55, speed = 30, wait = True);
-	sleep(vel);
+	sleep(sleepTime);
 	print("Drop!!!");
 	uArm.set_pump(False);	
 
 	print("Move to Dropping Point (Above)");
 	uArm.set_position(-100, 150, 150, speed = 50);
-	sleep(vel);
+	sleep(sleepTime);
 
+	uArm.set_buzzer(1000, 0.1)
 	print("Reset to Home Pose");
 	uArm.reset()
+
